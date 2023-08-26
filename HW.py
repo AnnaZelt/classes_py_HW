bc685afc96477df6cd3fcf97d2bd10a62d12436c
Anton Laptop <magicdude4@gmail.com>
2023-08-26 15:29:25 +0300

    first commit

---
+++ HW.py
@@ -0,0 +1,19 @@
+class Car:
+    color = ""
+    model = 0
+    carType = ""
+
+class Student:
+    name = ""
+    age = 0
+    avg = 0
+
+class Animal:
+    kingdom = ""
+    species = ""
+    color = ""
+
+class Computer:
+    cpu = ""
+    gpu = ""
+    ram = ""
---
+++ app.py
@@ -0,0 +1,22 @@
+class Peon:
+    color = ""
+    position = [None, None]
+
+class Queen:
+    color = ""
+    position = [None, None] 
+
+def move(peon):
+    peon.position = [int(input()),int(input())]
+    redraw()
+
+def redraw():
+    pass
+
+def getKilled(peon):
+    peon.color = ""
+    peon.position = [None, None]
+
+def Crowned(qPeon):
+    qPeon = Queen()
+    return qPeon
