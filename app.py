7b0005d616feac450b00e28ff8874bda70ccb429
Anton Laptop <magicdude4@gmail.com>
2023-08-26 15:53:25 +0300

    chees tweaks

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ class Queen:
     position = [None, None] 
 
 def move(peon):
+    peon = Peon()
     peon.position = [int(input()),int(input())]
     redraw()
 
@@ -14,9 +15,9 @@ def redraw():
     pass
 
 def getKilled(peon):
+    peon = Peon()
     peon.color = ""
     peon.position = [None, None]
 
 def Crowned(qPeon):
     qPeon = Queen()
-    return qPeon
